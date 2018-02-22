a92b1145c69142b73a39cd064235912a523d7aaa
Ken Kundert <ken@theKunderts.net>
2018-02-21 16:13:08 -0800

    Add useful information to install script.

--- setup.py
+++ setup.py
@@ -4,13 +4,31 @@ except ImportError:
     from distutils.core import setup
 import sys
 
+with open('README.rst') as f:
+    readme = f.read()
+
 setup(
     name='svg_schematic',
     version='0.1.0',
     description='svg schematics',
+    long_description=readme,
+    url='https://github.com/kenkundert/svg_schematic',
+    download_url='https://github.com/kenkundert/svg_schematic/tarball/master',
     author="Ken Kundert",
+    author_email='quantiphy@nurdletech.com',
     license='GPLv3+',
     zip_safe=True,
     py_modules='svg_schematic'.split(),
     install_requires='svgwrite'.split(),
+    keywords='svg schematic latex'.split(),
+    classifiers='''
+        Development Status :: 5 - Production/Stable
+        Intended Audience :: Science/Research
+        License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)
+        Natural Language :: English
+        Operating System :: POSIX :: Linux
+        Programming Language :: Python :: 3.6
+        Topic :: Utilities
+        Topic :: Scientific/Engineering
+    '''.strip().split('\n'),
 )
