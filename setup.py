37741bf6404c115c06afffdd5c4b4a4fbe0a2eb7
Ken Kundert <ken@theKunderts.net>
2020-04-09 18:24:36 -0700

    Nits.

--- setup.py
+++ setup.py
@@ -9,22 +9,22 @@ with open('README.rst') as f:
     readme = f.read()
 
 setup(
-    name='svg_schematic',
-    version='0.7.3',
-    description='svg schematics',
-    long_description=readme,
-    long_description_content_type='text/x-rst',
-    url='https://svg_schematic.readthedocs.io',
-    download_url='https://github.com/kenkundert/svg_schematic/tarball/master',
-    author="Ken Kundert",
-    author_email='quantiphy@nurdletech.com',
-    license='GPLv3+',
-    zip_safe=True,
-    py_modules='svg_schematic'.split(),
-    install_requires='svgwrite inform'.split(),
-    python_requires='>=3.6',
-    keywords='svg schematic latex'.split(),
-    classifiers=dedent('''
+    name = 'svg_schematic',
+    version = '0.7.3',
+    description = 'svg schematics',
+    long_description = readme,
+    long_description_content_type = 'text/x-rst',
+    url = 'https://svg_schematic.readthedocs.io',
+    download_url = 'https://github.com/kenkundert/svg_schematic/tarball/master',
+    author = "Ken Kundert",
+    author_email = 'quantiphy@nurdletech.com',
+    license = 'GPLv3+',
+    zip_safe = False,
+    py_modules = 'svg_schematic'.split(),
+    install_requires = 'svgwrite inform'.split(),
+    python_requires = '>=3.6',
+    keywords = 'svg schematic latex'.split(),
+    classifiers = dedent('''
         Development Status :: 5 - Production/Stable
         Intended Audience :: Science/Research
         License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)
